2255dac2a72973ff8e6e483adbc69ea21818c865
travistran <travistran544@gmail.com>
2023-07-27 10:08:35 -0400

    done?

--- heading_control.py
+++ heading_control.py
@@ -82,7 +82,7 @@ def main():
         desired_heading = np.deg2rad(desired_heading_deg-360)
 
 
-    pid = PID(10, 0.1, 10, 100)
+    pid = PID(8, 0.5, 8, 100)
 
     while True:
         # get yaw from the vehicle
